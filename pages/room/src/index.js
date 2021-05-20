0c2cfe764a699aa41df2914b3f13ef7d8063259d
HenriqueOmena <88lmao@gmail.com>
2021-05-20 22:47:21 +0100

    add room data and start socket file

---
+++ pages/room/src/index.js
@@ -0,0 +1,9 @@
+const room = {
+  id: Date.now(),
+  topic: "Js Expert",
+};
+
+const user = {
+  img: "https://raw.githubusercontent.com/halwax/halwax.github.io/master/images/avatar.png",
+  username: "Henrique Omena",
+};
