080e68e6889d7593ef7a1068b403ed20a5e45cf9
HenriqueOmena <88lmao@gmail.com>
2021-05-21 22:27:37 +0100

    Create base to connect with socket io

--- pages/room/src/index.js
+++ pages/room/src/index.js
@@ -1,3 +1,16 @@
+import { constants } from "../../_shared/constants";
+import SocketBuilder from "../../_shared/socketBuilder";
+
+const socket = new SocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNameSpaces.room,
+});
+
+socket
+  .setOnUserConnected((user) => console.log("user connected", user))
+  .setOnUserDisconnected((user) => console.log("user disconected", use))
+  .build();
+
 const room = {
   id: Date.now(),
   topic: "Js Expert",
@@ -7,3 +20,5 @@ const user = {
   img: "https://raw.githubusercontent.com/halwax/halwax.github.io/master/images/avatar.png",
   username: "Henrique Omena",
 };
+
+socket.emit(constants.events.JOIN_ROOM, { user, room });
